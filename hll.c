da9be834364f0cecefd9b8a24eaff1e9bd4f7f36
raduqq <radustefan.minea@yahoo.com>
2020-04-21 13:08:58 +0300

    Performing checks

--- hll.c
+++ hll.c
@@ -4,17 +4,11 @@
 #define ERROR_STATUS -1
 #define BITS_PER_BYTE 8
 #define NO_BUCKETS 2 << 11
-
-typedef struct info {
-    char *key;
-    char value;
-} info;
+#define BUCKET_BITS 11
 
 typedef struct Hashtable {
-    info *buckets;
     int hmax;
     unsigned int (*hash_function)(void*);
-    int (*compare_function)(void*, void*);
 } Hashtable;
 
 void file_check(FILE *f) {
@@ -24,59 +18,19 @@ void file_check(FILE *f) {
     }
 }
 
-void init_ht(Hashtable *ht, int hmax, unsigned int (*hash_function)(void*), int (*compare_function)(void*, void*)) {
+void init_ht(Hashtable *ht, int hmax, unsigned int (*hash_function)(void*)) {
     if (ht == NULL) {
         return;
     }
 
     ht->hmax = hmax;
     ht->hash_function = hash_function;
-    ht->compare_function = compare_function;
-
-    // Alocarea si initialziarea listelor inlantuite
-    ht->buckets = calloc(hmax, sizeof(info));
-}
-
-int compare_function_ints(void *a, void *b) {
-    int int_a = *((int *)a);
-    int int_b = *((int *)b);
-
-    if (int_a == int_b) {
-        return 0;
-    } else if (int_a < int_b) {
-        return -1;
-    } else {
-        return 1;
-    }
-}
-
-void free_ht(Hashtable *ht) {
-    if (ht == NULL) {
-        return;
-    }
-
-    for (int i = 0; i < ht->hmax; i++) {
-        free(ht->buckets[i].key);
-    }
-
-    free(ht->buckets);
-    free(ht);
 }
 
 void mem_check(void *p) {
     if (p == NULL) {
         fprintf(stderr, "Error allocating memory\n");
         exit(ERROR_STATUS);
-    }
-}
-
-int log_2(int x) {
-    /*
-    * Credits: https://stackoverflow.com/questions/14767308/how-to-computer-log-base-2-using-bitwise-operators
-    */
-    int answer = 0;
-    while(x >>= 1) {
-        answer++;
     }
 }
 
@@ -125,6 +79,7 @@ int main(int argc, char **argv) {
         exit(ERROR_STATUS);
     }
  
+    printf("%s\n", argv[1]);
     FILE *in = fopen(argv[1], "r");
     file_check(in);
 
@@ -132,28 +87,29 @@ int main(int argc, char **argv) {
     int no_bits_total = sizeof(int) * BITS_PER_BYTE;
     int bucket_no, zeros_cnt, x, no, hash;
     int m = NO_BUCKETS;
-    int log_m = log_2(m);
     int *M = calloc(m + 1, sizeof(int));
     mem_check(M);
 
     Hashtable *ht = malloc(sizeof(Hashtable));
     mem_check(ht);
-    init_ht(ht, m, hash_function_int, compare_function_ints);
+    init_ht(ht, m, hash_function_int);
 
     // 2) Processing input
     while(fscanf(in, "%d", &x) != EOF) {
         hash = ht->hash_function(&x);
- 
-        bucket_no = hash >> (no_bits_total - log_m);
-        zeros_cnt = find_initial_zeros(x, no_bits_total, log_m);
-        
+        if (hash < 0) {
+            hash *= -1;
+        }
+
+        bucket_no = hash >> (no_bits_total - BUCKET_BITS); // WARNING!
+        zeros_cnt = find_initial_zeros(hash, no_bits_total, BUCKET_BITS);
         M[bucket_no] = max(M[bucket_no], zeros_cnt);
     }
 
     // 3) Aggregating values
     double z = 0;
-
     for (int i = 0; i < m; i++) {
+
         z += (double)1 / (2 << M[i]);
     }
 
@@ -169,6 +125,6 @@ int main(int argc, char **argv) {
 
     fclose(in);
     free(M);
-    free_ht(ht);
+    free(ht);
     return 0;
 }
